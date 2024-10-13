0daa076754ddd46f63d1400f39c85395ea09d676
Plamen Ivanov <paco.iwanow@gmail.com>
2024-10-13 22:43:46 +0300

    refactor(models.py): switch from Document to Model in User, Course, and Enrollment classes feat(models.py): add is_admin field to User class fix(models.py): rename fields to follow snake_case convention feat(models.py): add foreign key constraints in Enrollment class

--- application/models.py
+++ application/models.py
@@ -1,14 +1,15 @@
-import flask
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from application import db
-from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(db.Document):
-    user_id = db.IntField(unique=True)
-    first_name = db.StringField(max_length=50)
-    last_name = db.StringField(max_length=50)
-    email = db.StringField(max_length=30, unique=True)
-    password = db.StringField()
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -17,14 +18,16 @@ class User(db.Document):
         return check_password_hash(self.password, password)
 
 
-class Course(db.Document):
-    courseID = db.StringField(max_length=10, unique=True)
-    title = db.StringField(max_length=100)
-    description = db.StringField(max_length=255)
-    credits = db.IntField()
-    term = db.StringField(max_length=25)
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    course_id = db.Column(db.String(10), unique=True, nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    credits = db.Column(db.Integer, nullable=False)
+    term = db.Column(db.String(25), nullable=False)
 
 
-class Enrollment(db.Document):
-    user_id = db.IntField()
-    courseID = db.StringField(max_length=10)
+class Enrollment(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey("course.id"), nullable=False)
