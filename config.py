4a7e5e62ae92197abaaf8a880da5befa3b88ad03
Plamen Ivanov <paco.iwanow@gmail.com>
2024-10-13 22:41:35 +0300

    refactor(config.py): simplify SECRET_KEY assignment and replace MONGODB_SETTINGS with SQLALCHEMY configurations

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 import os
 
 
-class Config(object):
-    SECRET_KEY = (
-        os.environ.get("SECRET_KEY")
-        or b"@\xdau\x8f\x97\x1d\x19$L\x0e\x08\xd4F\xc5\xfe\xea"
-    )
-
-    MONGODB_SETTINGS = {"db": "UTA_Enrollment"}
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///site.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
